77a44223f13cf6dca0c3470620b40574576bc810
Kazu <111069418+Kazu-K0032@users.noreply.github.com>
2025-10-12 15:46:11 +0900

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@/styles/globals.css';
+import { Metadata } from 'next';
+import {
+  SITE_TITLE,
+  SITE_DESCRIPTION,
+  INTER_FONT,
+} from '@/constants/globals.constants';
+import '@/styles/globals.css';
+
+export const metadata: Metadata = {
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={`${INTER_FONT.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/constants/globals.constants.ts
@@ -0,0 +1,25 @@
+import { Inter } from 'next/font/google';
+
+export const SITE_TITLE = 'Template App';
+export const SITE_DESCRIPTION = 'Webアプリテンプレート';
+
+export const INTER_FONT = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+  preload: true,
+  fallback: ['system-ui', 'arial'],
+});
+
+// コンテンツ設定
+export type ContentKey =
+  (typeof CONTENT_CONFIG)[keyof typeof CONTENT_CONFIG]['key'];
+export type ContentPath =
+  (typeof CONTENT_CONFIG)[keyof typeof CONTENT_CONFIG]['path'];
+export const CONTENT_CONFIG = {
+  HOME: {
+    key: 'home',
+    name: 'ホーム',
+    path: '/',
+  },
+} as const;
